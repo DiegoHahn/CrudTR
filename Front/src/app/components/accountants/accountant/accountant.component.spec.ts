e9f409a848fb2755355052be647651226bad07c8
Diego Hahn <diego-hf@hotmail.com>
2024-05-22 14:01:03 -0300

    adicionando teste no componente accuntant, e ignorando os outros arquivos de teste no package.json

--- Front/src/app/components/accountants/accountant/accountant.component.spec.ts
+++ Front/src/app/components/accountants/accountant/accountant.component.spec.ts
@@ -1,5 +1,6 @@
+import { TemplateRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Accountant } from '../accountant';
 import { AccountantComponent } from './accountant.component';
 
 describe('AccountantComponent', () => {
@@ -7,17 +8,36 @@ describe('AccountantComponent', () => {
   let fixture: ComponentFixture<AccountantComponent>;
 
   beforeEach(async () => {
+    // Configuração do módulo de teste 
     await TestBed.configureTestingModule({
       declarations: [ AccountantComponent ]
     })
     .compileComponents();
-
+  });
+  
+  beforeEach(() => {
     fixture = TestBed.createComponent(AccountantComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  // Teste de criação do componente
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  // Teste para verificar se a entrada de listAccountants é aceita
+  it('should accept listAccountants input', () => {
+    const accountants: Accountant[] = [
+      { name: "teste1", registrationNumber: "10832323222", accountantCode: "123", isActive: true },
+      { name: "teste2", registrationNumber: "12345678901", accountantCode: "123", isActive: false }
+    ];
+    component.listAccountants = accountants;
+    expect(component.listAccountants).toEqual(accountants);
+  });
+  // Teste para verificar se a referência do template de ação é definida
+  it('should set action template reference', () => {
+    const templateRef: TemplateRef<Accountant> = {} as TemplateRef<Accountant>;
+    component.actionTemplateRef = templateRef;
+    expect(component.actionTemplateRef).toBe(templateRef);
   });
 });
