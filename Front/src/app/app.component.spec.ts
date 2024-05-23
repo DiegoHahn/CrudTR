4267db47aa2ff678e27e096be541911d999f2a7a
Diego Hahn <diego-hf@hotmail.com>
2024-05-23 08:35:45 -0300

    adicionando teste de unidade para o AppComponent

--- Front/src/app/app.component.spec.ts
+++ Front/src/app/app.component.spec.ts
@@ -1,17 +1,48 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 describe('AppComponent', () => {
-  let fixture: AppComponent;
-
-  beforeEach(() => {
-    fixture = new AppComponent();
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      //importa o módulo de teste de roteador para simular a navegação
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
+      ],
+    }).compileComponents();
+  }));
 
   it('should create the app', () => {
-    expect(fixture).toBeTruthy();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
-  it(`should have as title CrudTR`, () => {
-    expect(fixture.title).toEqual('CrudTR');
+  it(`should have as title 'CrudTR'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('CrudTR');
+  });
+
+  it('should render header', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    //verifica se o componente foi renderizado
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-header')).not.toBe(null);
+  });
+
+  it('should render footer', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-footer')).not.toBe(null);
   });
 });
